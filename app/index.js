0ef5d6f7e950959c8f463ac648a9c13060ed7832
Anders Nilsson <nilssonanders79@gmail.com>
2020-11-24 13:09:01 +0100

    react

--- app/index.js
+++ app/index.js
@@ -1,69 +1,121 @@
-const Generator = require("yeoman-generator");
+const Generator = require('yeoman-generator');
+const path = require('path');
+const fs = require('fs');
 
-module.exports = class extends Generator {
+const promisedExist = (pathToCheck) => {
+  return new Promise((resolve, reject) => {
+    fs.stat(pathToCheck, function (err, stats) {
+      if (stats && stats.isDirectory()) {
+        resolve(true);
+      }
+      resolve(false);
+    });
+  });
+};
+
+module.exports = class extends (
+  Generator
+) {
+  async initializing() {
+    const isGitDirectory = await promisedExist(
+      path.join(process.cwd(), '.git')
+    );
+    if (!isGitDirectory) {
+      this.log(
+        "not a git directory, run 'git init' before running this generator"
+      );
+      process.exit(0);
+    }
+  }
+
   async prompting() {
     this.answers = await this.prompt([
       {
-        type: "input",
-        name: "name",
-        message: "Your project name",
-        default: this.appname
-      }
+        type: 'input',
+        name: 'name',
+        message: 'Your project name',
+        default: path.basename(process.cwd()),
+      },
+      {
+        type: 'list',
+        name: 'npm_yarn',
+        message: 'use npm or yarn',
+        choices: ['npm', 'yarn'],
+        default: 'npm',
+      },
     ]);
   }
   writing() {
-    const templates = ["package.json", "README.md", "webpack.config.js"];
-    templates.forEach(template => {
+    const templates = ['package.json', 'README.md', 'webpack.config.js'];
+    templates.forEach((template) => {
       this.fs.copyTpl(
         this.templatePath(template),
         this.destinationPath(template),
         {
           name: this.answers.name,
           gitname: this.user.git.name(),
-          gitemail: this.user.git.email()
+          gitemail: this.user.git.email(),
         } // user answer `title` used
       );
     });
     const files = [
-      "public/favicon.ico",
-      "public/index.ejs",
-      "public/manifest.json",
-      "src/index.js",
-      ".editorconfig",
-      ".eslintignore",
-      ".gitignore",
-      ".prettierrc",
-      ".prettierignore",
-      "LICENSE"
+      'public/favicon.ico',
+      'public/index.ejs',
+      'public/manifest.json',
+      'src/index.jsx',
+      'src/App.jsx',
+      '.babelrc',
+      '.editorconfig',
+      '.eslintrc',
+      '.eslintignore',
+      '.gitignore',
+      '.prettierrc',
+      '.prettierignore',
+      'LICENSE',
     ];
-    files.forEach(file => {
+    files.forEach((file) => {
       this.fs.copy(this.templatePath(file), this.destinationPath(file));
     });
   }
   installingDeps() {
-    this.yarnInstall(
-      [
-        "@babel/core",
-        "@babel/preset-env",
-        "babel-loader",
-        "copy-webpack-plugin",
-        "eslint",
-        "eslint-config-airbnb-base",
-        "eslint-config-prettier",
-        "eslint-plugin-import",
-        "eslint-plugin-prettier",
-        "html-webpack-plugin",
-        "husky",
-        "prettier",
-        "pretty-quick",
-        "webpack",
-        "webpack-cli",
-        "webpack-dev-server"
-      ],
-      { dev: true }
-    );
+    const devDeps = [
+      '@babel/core',
+      '@babel/preset-env',
+      '@babel/preset-react',
+      'babel-loader',
+      'copy-webpack-plugin',
+      'eslint',
+      'eslint-config-airbnb',
+      'eslint-config-prettier',
+      'eslint-plugin-import',
+      'eslint-plugin-jsx-a11y',
+      'eslint-plugin-prettier',
+      'eslint-plugin-react',
+      'eslint-plugin-react-hooks',
+      'html-webpack-plugin',
+      'husky',
+      'prettier',
+      'pretty-quick',
+      'webpack',
+      'webpack-cli',
+      'webpack-dev-server',
+    ];
+    const deps = ['react', 'react-dom', 'prop-types'];
+    if (this.answers.npm_yarn === 'npm') {
+      this.npmInstall(devDeps, { 'save-dev': true });
+      this.npmInstall(deps, { 'save-dev': false });
+    } else {
+      this.yarnInstall(devDeps, { dev: true });
+      this.yarnInstall(deps, { dev: false });
+    }
   }
-  done() {
-    this.log("start development with `yarn start`");
+  end() {
+    if (this.answers.npm_yarn === 'npm') {
+      this.log('build with `npm run build`');
+      this.log('start development with `npm start`');
+    } else {
+      this.log('build with `yarn run build`');
+      this.log('start development with `yarn start`');
+    }
   }
 };
--- app/templates/webpack.config.js
+++ app/templates/webpack.config.js
@@ -3,22 +3,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env) => ({
-  entry: path.resolve(__dirname, 'src/index.js'),
-  devtool: 'sourcemap',
+  entry: path.resolve(__dirname, 'src/index.jsx'),
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: [{ loader: 'babel-loader' }],
       },
     ],
   },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'eval-source-map',
   devServer: {
+    stats: 'minimal',
     historyApiFallback: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
+    contentBase: path.resolve(__dirname, './dist'),
   },
   stats: 'minimal',
   plugins: [
@@ -27,6 +29,7 @@ module.exports = (env) => ({
       templateParameters: {
         name: '<%= name %>',
       },
+      scriptLoading: 'defer',
     }),
     new CopyWebpackPlugin({
       patterns: [
