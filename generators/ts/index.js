1b655c21fc84527075119f86aa148c1604deffbc
Anders Nilsson <nilssonananders79@gmail.com>
2021-01-04 13:10:36 +0100

    fix: .gitignore problems

--- generators/ts/index.js
+++ generators/ts/index.js
@@ -1,14 +1,19 @@
-const Generator = require('yeoman-generator');
+const Generator = require("yeoman-generator");
 
 module.exports = class extends (
   Generator
 ) {
   writing() {
-    const templates = ['tmpl_package.json', 'README.md', 'webpack.config.js'];
+    const templates = [
+      "tmpl_package.json",
+      "tmpl_.gitignore",
+      "README.md",
+      "webpack.config.js",
+    ];
     templates.forEach((template) => {
       this.fs.copyTpl(
         this.templatePath(template),
-        this.destinationPath(template.replace('tmpl_', '')),
+        this.destinationPath(template.replace("tmpl_", "")),
         {
           name: this.options.name,
           gitname: this.options.gitname,
@@ -17,23 +22,22 @@ module.exports = class extends (
       );
     });
     const files = [
-      'public/favicon.ico',
-      'public/manifest.json',
-      'src/App.tsx',
-      'src/check.gif',
-      'src/index.ejs',
-      'src/index.ts',
-      'src/main.css',
-      'src/declarations.d.ts',
-      'tsconfig.json',
-      '.babelrc',
-      '.editorconfig',
-      '.eslintrc',
-      '.eslintignore',
-      '.gitignore',
-      '.prettierrc',
-      '.prettierignore',
-      'LICENSE',
+      "public/favicon.ico",
+      "public/manifest.json",
+      "src/App.tsx",
+      "src/check.gif",
+      "src/index.ejs",
+      "src/index.ts",
+      "src/main.css",
+      "src/declarations.d.ts",
+      "tsconfig.json",
+      ".babelrc",
+      ".editorconfig",
+      ".eslintrc",
+      ".eslintignore",
+      ".prettierrc",
+      ".prettierignore",
+      "LICENSE",
     ];
     files.forEach((file) => {
       this.fs.copy(this.templatePath(file), this.destinationPath(file));
@@ -41,40 +45,40 @@ module.exports = class extends (
   }
   installingDeps() {
     const devDeps = [
-      '@babel/core',
-      '@babel/preset-env',
-      '@babel/preset-react',
-      '@babel/preset-typescript',
-      '@types/react',
-      '@types/react-dom',
-      '@typescript-eslint/eslint-plugin',
-      '@typescript-eslint/parser',
-      'babel-loader',
-      'clean-webpack-plugin',
-      'copy-webpack-plugin',
-      'css-loader',
-      'eslint',
-      'eslint-config-airbnb-typescript',
-      'eslint-config-prettier',
-      'eslint-plugin-import',
-      'eslint-plugin-jsx-a11y',
-      'eslint-plugin-prettier',
-      'eslint-plugin-react',
-      'eslint-plugin-react-hooks',
-      'html-webpack-plugin',
-      'husky',
-      'prettier',
-      'pretty-quick',
-      'style-loader',
-      'typescript',
-      'webpack',
-      'webpack-cli',
-      'webpack-dev-server',
+      "@babel/core",
+      "@babel/preset-env",
+      "@babel/preset-react",
+      "@babel/preset-typescript",
+      "@types/react",
+      "@types/react-dom",
+      "@typescript-eslint/eslint-plugin",
+      "@typescript-eslint/parser",
+      "babel-loader",
+      "clean-webpack-plugin",
+      "copy-webpack-plugin",
+      "css-loader",
+      "eslint",
+      "eslint-config-airbnb-typescript",
+      "eslint-config-prettier",
+      "eslint-plugin-import",
+      "eslint-plugin-jsx-a11y",
+      "eslint-plugin-prettier",
+      "eslint-plugin-react",
+      "eslint-plugin-react-hooks",
+      "html-webpack-plugin",
+      "husky",
+      "prettier",
+      "pretty-quick",
+      "style-loader",
+      "typescript",
+      "webpack",
+      "webpack-cli",
+      "webpack-dev-server",
     ];
-    const deps = ['react', 'react-dom', 'prop-types'];
+    const deps = ["react", "react-dom", "prop-types"];
     if (this.options.npm) {
-      this.npmInstall(devDeps, { 'save-dev': true });
-      this.npmInstall(deps, { 'save-dev': false });
+      this.npmInstall(devDeps, { "save-dev": true });
+      this.npmInstall(deps, { "save-dev": false });
     } else if (this.options.yarn) {
       this.yarnInstall(devDeps, { dev: true });
       this.yarnInstall(deps, { dev: false });
